6d00b4ed2287e390896d60a99a64861c1277e68e
Legendary-Overlord <santiagoz9907@gmail.com>
2020-05-23 21:32:06 -0500

    Added js for sectors

--- src/main/java/services/SimulationService.java
+++ src/main/java/services/SimulationService.java
@@ -1,9 +1,13 @@
 package services;
 
+        import db.ConnectionPool;
+        import db.IcesiDatabase;
+        import model.Measurement;
         import model.Simulation;
 
         import javax.ejb.Stateless;
         import javax.ws.rs.*;
+        import java.lang.reflect.Array;
         import java.util.ArrayList;
 
 @Stateless
@@ -17,5 +21,28 @@ public class SimulationService {
         Simulation simulation = new Simulation();
         System.out.println(mode.charAt(0)+" "+periodNum);
         return simulation.simulate(mode.charAt(0), Integer.parseInt(periodNum));
+    }
+    @GET
+    @Path("get/data/{sector}")
+    @Produces("application/json")
+    public ArrayList<ArrayList<Double>> getData(@PathParam("sector")String sector) {
+        IcesiDatabase icesiDatabase = ConnectionPool.getAvailableConnection();
+        ArrayList<Measurement> measurements = icesiDatabase.getListMeasurement(sector);
+        ArrayList<ArrayList<Double>> arrayLists = new ArrayList<>();
+        ArrayList<Double> ph = new ArrayList<>();
+        ArrayList<Double> hum = new ArrayList<>();
+        ArrayList<Double> co2 = new ArrayList<>();
+        ArrayList<Double> temperature = new ArrayList<>();
+        measurements.forEach(e -> {
+            ph.add(e.getpH());
+            hum.add(e.getHumidity());
+            co2.add(e.getCo2());
+            temperature.add(e.getTemperature());
+        });
+        arrayLists.add(ph);
+        arrayLists.add(hum);
+        arrayLists.add(co2);
+        arrayLists.add(temperature);
+        return arrayLists;
     }
 }
